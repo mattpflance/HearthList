18df138e5325bddab35cd9021878fa015a36e914
MattPflance <matthewpflance@gmail.com>
2016-05-07 19:07:44 -0400

    Feature: Added enums for common types in the game.

---
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/enums/CardRarity.java
@@ -0,0 +1,8 @@
+package com.mattpflance.hearthlist.enums;
+
+/**
+ * The rarities of cards in Hearthstone
+ */
+public enum CardRarity {
+    FREE, COMMON, RARE, EPIC, LEGENDARY
+}
---
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/enums/PlayerClass.java
@@ -0,0 +1,8 @@
+package com.mattpflance.hearthlist.enums;
+
+/**
+ * Classes that are available in HearthStone
+ */
+public enum PlayerClass {
+    DRUID, HUNTER, MAGE, PALADIN, PRIEST, ROGUE, SHAMAN, WARLOCK, WARRIOR, NEUTRAL
+}
