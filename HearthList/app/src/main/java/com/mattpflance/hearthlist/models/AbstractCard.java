d747569535582c6d4f10bfc1b888db0391fe978c
MattPflance <matthewpflance@gmail.com>
2016-05-08 16:36:48 -0400

    Feature/Refactor: Abstracted more data from Minions.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/models/AbstractCard.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/models/AbstractCard.java
@@ -1,13 +1,16 @@
 package com.mattpflance.hearthlist.models;
 
+import android.graphics.Bitmap;
+
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A Class that stores relevant data for HearthStone cards.
  */
 public abstract class AbstractCard {
 
-    protected final int ABSTRACT_CARD_VAR_COUNT = 6;
+    protected final int ABSTRACT_CARD_VAR_COUNT = 10;
 
     protected String mId;
     protected String mName;
@@ -15,14 +18,26 @@ public abstract class AbstractCard {
     protected String mCardRarity;
     protected String mPlayerClass;
     protected boolean mCollectible;
+    protected int mManaCost;
+    protected String mArtist;
+    protected String mText;
+    protected String mFlavorText;
+    protected List<String> mMechanics;
+    protected Bitmap mRegImg;
+    protected Bitmap mGoldImg;
 
-    protected AbstractCard(ArrayList<String> params) {
+    protected AbstractCard(ArrayList<String> params, List<String> mechanics) {
         mId = params.get(0);
         mName = params.get(1);
         mCardSet = params.get(2);
         mCardRarity = params.get(3);
         mPlayerClass = params.get(4);
         mCollectible = Boolean.parseBoolean(params.get(5));
+        mManaCost = Integer.parseInt(params.get(6));
+        mArtist = params.get(7);
+        mText = params.get(8);
+        mFlavorText = params.get(9);
+        mMechanics = mechanics;
     }
 
 }
--- HearthList/app/src/main/java/com/mattpflance/hearthlist/models/Minion.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/models/Minion.java
@@ -5,24 +5,16 @@ import java.util.List;
 
 public class Minion extends AbstractCard {
 
-    private String mArtist;
-    private String mText;
-    private String mFlavorText;
     private String mRace;
     private int mAttack;
     private int mHealth;
-    private List<String> mMechanics;
 
-    public Minion(ArrayList<String> params){
-        super(params);
+    public Minion(ArrayList<String> params, List<String> mechanics){
+        super(params, mechanics);
 
-        mArtist = params.get(ABSTRACT_CARD_VAR_COUNT);
-        mText = params.get(1 + ABSTRACT_CARD_VAR_COUNT);
-        mFlavorText = params.get(2 + ABSTRACT_CARD_VAR_COUNT);
-        mRace = params.get(3 + ABSTRACT_CARD_VAR_COUNT);
-        mAttack = Integer.parseInt(params.get(4 + ABSTRACT_CARD_VAR_COUNT));
-        mHealth = Integer.parseInt(params.get(5 + ABSTRACT_CARD_VAR_COUNT));
-        mMechanics = params.subList(6 + ABSTRACT_CARD_VAR_COUNT, params.size());
+        mRace = params.get(ABSTRACT_CARD_VAR_COUNT);
+        mAttack = Integer.parseInt(params.get(1 + ABSTRACT_CARD_VAR_COUNT));
+        mHealth = Integer.parseInt(params.get(2 + ABSTRACT_CARD_VAR_COUNT));
     }
 
 }
