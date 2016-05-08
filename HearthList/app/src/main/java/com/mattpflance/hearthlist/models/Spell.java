559b324439291c281332a1d3ac1d3b8b29e1ebb3
MattPflance <matthewpflance@gmail.com>
2016-05-08 16:37:10 -0400

    Feature: Created a Spell class that implements the AbstractCard class.

---
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/models/Spell.java
@@ -0,0 +1,12 @@
+package com.mattpflance.hearthlist.models;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Spell extends AbstractCard {
+
+    public Spell(ArrayList<String> params, List<String> mechanics){
+        super(params, mechanics);
+    }
+
+}
