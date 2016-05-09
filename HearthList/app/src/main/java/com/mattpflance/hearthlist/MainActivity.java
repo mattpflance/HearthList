b62a3513ef94c720c1877263ebeade2a1fd7ac1c
MattPflance <matthewpflance@gmail.com>
2016-05-08 20:11:30 -0400

    Feature: Set up TabLayout, PagerAdapter, ViewPager and removed DrawerLayout.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/MainActivity.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/MainActivity.java
@@ -1,15 +1,17 @@
 package com.mattpflance.hearthlist;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.util.Log;
 import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
 
 import java.io.IOException;
 
@@ -19,19 +21,26 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements
+        PagerFragment.OnFragmentInteractionListener,
+        CardsFragment.OnFragmentInteractionListener,
+        DecksFragment.OnFragmentInteractionListener {
 
     private OkHttpClient mClient;
-    private TextView mTextView;
+    private PagerAdapter mPagerAdapter;
+
+    private TabLayout mTabLayout;
+    private ViewPager mViewPager;
+    private PagerFragment mPagerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mTextView = (TextView) findViewById(R.id.text_view);
         mClient = new OkHttpClient();
 
         try {
@@ -39,6 +48,19 @@ public class MainActivity extends AppCompatActivity {
         } catch (IOException e) {
             e.printStackTrace();
         }
+
+        mPagerFragment = (PagerFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.view_pager_fragment);
+
+        mPagerAdapter = new PagerAdapter(getSupportFragmentManager(), this);
+
+        mViewPager = (ViewPager) mPagerFragment.getView();
+        mViewPager.setAdapter(mPagerAdapter);
+
+        mTabLayout = (TabLayout) findViewById(R.id.tab_layout);
+        mTabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        mTabLayout.setupWithViewPager(mViewPager);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         if (fab != null) {
@@ -51,14 +73,6 @@ public class MainActivity extends AppCompatActivity {
             });
         }
 
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-//                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-//        drawer.setDrawerListener(toggle);
-//        toggle.syncState();
-//
-//        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-//        navigationView.setNavigationItemSelectedListener(this);
     }
 
     private void getCardsAsync() throws IOException {
@@ -79,26 +93,10 @@ public class MainActivity extends AppCompatActivity {
                         if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
 
                         final String res = response.body().string();
-
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                mTextView.setText("got em");
-                            }
-                        });
                     }
                 });
     }
 
-//    @Override
-//    public void onBackPressed() {
-//        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-//        if (drawer.isDrawerOpen(GravityCompat.START)) {
-//            drawer.closeDrawer(GravityCompat.START);
-//        } else {
-//            super.onBackPressed();
-//        }
-//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -120,5 +118,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onFragmentInteraction(Uri uri){
+
     }
 }
