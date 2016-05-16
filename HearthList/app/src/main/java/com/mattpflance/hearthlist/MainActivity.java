1f470420efa3f8ba3766f5e7fdebab28c8bbbf9a
MattPflance <matthewpflance@gmail.com>
2016-05-15 23:26:36 -0400

    Fix: fixed crash from removing FAB.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/MainActivity.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/MainActivity.java
@@ -54,16 +54,16 @@ public class MainActivity extends AppCompatActivity implements
         mTabLayout.setupWithViewPager(mViewPager);
 
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        if (fab != null) {
-            fab.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                            .setAction("Action", null).show();
-                }
-            });
-        }
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        if (fab != null) {
+//            fab.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View view) {
+//                    Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                            .setAction("Action", null).show();
+//                }
+//            });
+//        }
 
     }
 
