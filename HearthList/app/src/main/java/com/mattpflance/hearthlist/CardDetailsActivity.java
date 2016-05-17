a0d788e57c190fadf889e713c1bf25aab5beadae
MattPflance <matthewpflance@gmail.com>
2016-05-17 19:25:33 -0400

    Feature: Added CardDetails Fragment/Activity classes and layouts that display the card image.

---
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsActivity.java
@@ -0,0 +1,49 @@
+package com.mattpflance.hearthlist;
+
+import android.content.Intent;
+import android.database.Cursor;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.mattpflance.hearthlist.data.HearthListContract;
+import com.mattpflance.hearthlist.models.Card;
+
+public class CardDetailsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_details);
+
+        if (findViewById(R.id.card_details_fragment_container) != null) {
+
+            // However, if we're being restored from a previous state,
+            // then we don't need to do anything and should return or else
+            // we could end up with overlapping fragments.
+            if (savedInstanceState != null) {
+                return;
+            }
+
+            // Create a new Fragment to be placed in the activity layout
+            CardDetailsFragment cardDetailsFragment = CardDetailsFragment.newInstance();
+
+            // Create a Card
+            Card card = getIntent().getParcelableExtra("CARD");
+
+            Bundle bundle = new Bundle();
+            bundle.putParcelable("CARD", card);
+
+            // In case this activity was started with special instructions from an
+            // Intent, pass the Intent's extras to the fragment as arguments
+            cardDetailsFragment.setArguments(getIntent().getExtras());
+
+            // Add the fragment to the 'fragment_container' FrameLayout
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.card_details_fragment_container, cardDetailsFragment).commit();
+
+        }
+    }
+}
---
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsFragment.java
@@ -0,0 +1,65 @@
+package com.mattpflance.hearthlist;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.mattpflance.hearthlist.models.Card;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class CardDetailsFragment extends Fragment {
+
+    private Card mCard;
+
+    public CardDetailsFragment() {
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     */
+    public static CardDetailsFragment newInstance() {
+        CardDetailsFragment fragment = new CardDetailsFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mCard = getArguments().getParcelable("CARD");
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_card_details, container, false);
+
+        ImageView imageView = (ImageView) view.findViewById(R.id.card_image);
+
+        String imagePath = mCard.getRegImageURL();
+        if (imagePath != null) {
+            Glide.with(this)
+                    .load(imagePath)
+                    .diskCacheStrategy(DiskCacheStrategy.ALL) // Load from internal
+                    .into(imageView);
+        }
+
+        return view;
+    }
+
+}
