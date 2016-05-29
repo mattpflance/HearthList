de3078dc6076c86e11e652f2c92ade958d8f29b5
MattPflance <matthewpflance@gmail.com>
2016-05-29 14:28:18 -0400

    Feature: Added buttons on CardDetails to switch from Normal/Gold images.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsFragment.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsFragment.java
@@ -8,6 +8,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -22,6 +23,10 @@ import com.mattpflance.hearthlist.models.Card;
 public class CardDetailsFragment extends Fragment {
 
     private Card mCard;
+
+    private ImageView mCardImage;
+    private Button mRegImageButton;
+    private Button mGoldImageButton;
 
     public CardDetailsFragment() {
     }
@@ -50,15 +55,45 @@ public class CardDetailsFragment extends Fragment {
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_card_details, container, false);
 
-        ImageView imageView = (ImageView) view.findViewById(R.id.card_image);
-        //GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(imageView);
+        mCardImage = (ImageView) view.findViewById(R.id.card_image);
+
+        mRegImageButton = (Button) view.findViewById(R.id.reg_image_button);
+        mRegImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO change both button's background state to pressed when clicked
+                String imagePath = mCard.getRegImageUrl();
+                if (imagePath != null) {
+                    Glide.with(v.getContext())
+                            .load(imagePath)
+                            .diskCacheStrategy(DiskCacheStrategy.ALL) // Load from internal
+                            .into(mCardImage);
+                }
+            }
+        });
+
+        mGoldImageButton = (Button) view.findViewById(R.id.gold_image_button);
+        mGoldImageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO change both button's background state to pressed when clicked
+                String imagePath = mCard.getGoldImageUrl();
+                if (imagePath != null) {
+                    GlideDrawableImageViewTarget imageViewTarget = new GlideDrawableImageViewTarget(mCardImage);
+                    Glide.with(v.getContext())
+                            .load(mCard.getGoldImageUrl())
+                            .diskCacheStrategy(DiskCacheStrategy.ALL) // Load from internal
+                            .into(imageViewTarget);
+                }
+            }
+        });
+
         String imagePath = mCard.getRegImageUrl();
-        //String imagePath = mCard.getGoldImageUrl();
         if (imagePath != null) {
             Glide.with(this)
                     .load(imagePath)
                     .diskCacheStrategy(DiskCacheStrategy.ALL) // Load from internal
-                    .into(imageView);
+                    .into(mCardImage);
         }
 
         TextView textView = (TextView) view.findViewById(R.id.artist);
