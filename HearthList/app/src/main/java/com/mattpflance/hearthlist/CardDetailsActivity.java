6f1a8479ba62bd5b70dc5ccd2921b907ed641749
MattPflance <matthewpflance@gmail.com>
2016-06-04 14:34:23 -0400

    Refactor: Removed comments.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsActivity.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/CardDetailsActivity.java
@@ -1,14 +1,8 @@
 package com.mattpflance.hearthlist;
 
-import android.content.Intent;
-import android.database.Cursor;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import com.mattpflance.hearthlist.data.HearthListContract;
 import com.mattpflance.hearthlist.models.Card;
 
 public class CardDetailsActivity extends AppCompatActivity {
@@ -22,9 +16,6 @@ public class CardDetailsActivity extends AppCompatActivity {
 
         if (findViewById(R.id.card_details_fragment_container) != null) {
 
-            // However, if we're being restored from a previous state,
-            // then we don't need to do anything and should return or else
-            // we could end up with overlapping fragments.
             if (savedInstanceState != null) {
                 return;
             }
