e4112fdb77d7fcb509d79faefbbaf4baf0f82f3d
MattPflance <matthewpflance@gmail.com>
2016-06-04 12:16:25 -0400

    Fix: Added filter titles to strings.xml and fixed crash where CardFiltersFragment could not find title string.

--- HearthList/app/src/main/java/com/mattpflance/hearthlist/settings/CardFiltersFragment.java
+++ HearthList/app/src/main/java/com/mattpflance/hearthlist/settings/CardFiltersFragment.java
@@ -1,34 +1,15 @@
 package com.mattpflance.hearthlist.settings;
 
-
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
 import com.mattpflance.hearthlist.R;
 
 public class CardFiltersFragment extends PreferenceFragment {
 
-//    public static CardFiltersFragment newInstance(String param1, String param2) {
-//        CardFiltersFragment fragment = new CardFiltersFragment();
-//        Bundle args = new Bundle();
-//        args.putString(ARG_PARAM1, param1);
-//        args.putString(ARG_PARAM2, param2);
-//        fragment.setArguments(args);
-//        return fragment;
-//    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        if (getArguments() != null) {
-//            mParam1 = getArguments().getString(ARG_PARAM1);
-//            mParam2 = getArguments().getString(ARG_PARAM2);
-//        }
 
         // Load the preferences from an XML resource
         addPreferencesFromResource(R.xml.cards_filter_preferences);
